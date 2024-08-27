8390bd591b75e36c3901c9e125c2f6f2da331b31
daniyalrosli <94339715+daniyalrosli@users.noreply.github.com>
2024-08-27 09:32:00 +0800

    adding components  only

---
+++ src/app/component/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+    return (
+        <div>
+            <h2>About Me</h2>
+            <p>Write your about me content here.</p>
+        </div>
+    );
+};
+
+export default About;
